d15484688eb1a858c1b887d4c3d31927869aecea
DefiledSpec <defiledspec@gmail.com>
2018-08-02 16:21:46 -0500

    added submit/reset buttons

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,12 +1,12 @@
 import React, {Component} from 'react'
 import {withStyles} from '@material-ui/core/styles'
-import {Paper, TextField, Typography } from '@material-ui/core'
+import {Paper, TextField, Typography, Button } from '@material-ui/core'
 
 const styles = {
 	root: {
 		maxWidth: 'max-content',
 		margin: '2em auto',
-		padding: '1.5em 4em'
+		padding: '1.5em 3em'
 	},
 	textField: {
 		// marginLeft: theme.spacing.unit,
@@ -17,6 +17,9 @@ const styles = {
 	headline: {
 		margin: '.5em auto',
 		width: 'max-content'
+	},
+	submit: {
+		float: 'right'
 	}
 }
 
@@ -24,22 +27,27 @@ class Contact extends Component {
 	constructor(props) {
 		super(props)
 		this.state = {
-			name: ''
+			
 		}
 	}
 	handleChange = element => ({target}) => {
-		if(target.value)
+		console.log(target.value)
+		if(element) {
 			this.setState({
 				[element]: target.value
 			})
+		}
+	}
+	handleSubmit = e => {
+		console.log(e)
 	}
 	render() {
 		const {classes} = this.props
 		return (
 			<Paper className={classes.root}>
-			<Typography gutterBottom variant='headline' component='h3' className={classes.headline}>Contact Us</Typography>
+			<Typography gutterBottom variant='headline' component='h3' className={classes.headline}>Contact Me</Typography>
 			<hr/>
-				<form className={classes.container} noValidate autoComplete='off'>
+				<form className={classes.container} noValidate autoComplete='off' onSubmit={this.handleSubmit}>
 					<TextField id='name' label='Name' className={classes.textField} value={this.state.name} onChange={this.handleChange('name')} margin='normal' />
 					<br/>
 					<TextField id='email' label='Email' className={classes.textField} value={this.state.email} onChange={this.handleChange('email')} margin='normal' />
@@ -49,11 +57,14 @@ class Contact extends Component {
 						label="Multiline"
 						multiline
 						rowsMax="4"
-						value={this.state.multiline}
-						onChange={this.handleChange('multiline')}
+						value={this.state.message}
+						onChange={this.handleChange('message')}
 						className={classes.textField}
 						margin="normal"
 					/>
+					<br/>
+					<Button type='submit' color='secondary' variant='raised'>Reset</Button>
+					<Button type='submit' color='primary' variant='raised' className={classes.submit}>Submit</Button>
 				</form>
 			</Paper>
 		)
