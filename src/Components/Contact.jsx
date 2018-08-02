e4ad118e42480e181107f8c5c41e01376ddd1576
DefiledSpec <defiledspec@gmail.com>
2018-08-02 17:07:31 -0500

    started adding backend for contact, moving to new branch

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -27,11 +27,12 @@ class Contact extends Component {
 	constructor(props) {
 		super(props)
 		this.state = {
-			
+			name: '',
+			email: '',
+			message: '',			
 		}
 	}
 	handleChange = element => ({target}) => {
-		console.log(target.value)
 		if(element) {
 			this.setState({
 				[element]: target.value
@@ -39,7 +40,19 @@ class Contact extends Component {
 		}
 	}
 	handleSubmit = e => {
-		console.log(e)
+		e.preventDefault()
+		let { target } = e
+		let name = target[0].value
+		let email = target[1].value
+		let msg = target[3].value
+		console.log('name: %s\nemail: %s\nmsg: %s\n', name, email, msg)
+	}
+	handleReset = () => {
+		this.setState({
+			name: '',
+			email: '',
+			message: '',
+		})
 	}
 	render() {
 		const {classes} = this.props
@@ -47,7 +60,7 @@ class Contact extends Component {
 			<Paper className={classes.root}>
 			<Typography gutterBottom variant='headline' component='h3' className={classes.headline}>Contact Me</Typography>
 			<hr/>
-				<form className={classes.container} noValidate autoComplete='off' onSubmit={this.handleSubmit}>
+				<form className={classes.container} noValidate autoComplete='off' onSubmit={this.handleSubmit.bind(this)} onReset={this.handleReset}>
 					<TextField id='name' label='Name' className={classes.textField} value={this.state.name} onChange={this.handleChange('name')} margin='normal' />
 					<br/>
 					<TextField id='email' label='Email' className={classes.textField} value={this.state.email} onChange={this.handleChange('email')} margin='normal' />
@@ -61,9 +74,10 @@ class Contact extends Component {
 						onChange={this.handleChange('message')}
 						className={classes.textField}
 						margin="normal"
+						style={{marginBottom: '3em'}}
 					/>
 					<br/>
-					<Button type='submit' color='secondary' variant='raised'>Reset</Button>
+					<Button type='reset' color='secondary' variant='raised'>Reset</Button>
 					<Button type='submit' color='primary' variant='raised' className={classes.submit}>Submit</Button>
 				</form>
 			</Paper>
