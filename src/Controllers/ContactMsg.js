73e7cbf0a8997a4908d01dfad832ead96ce8eb16
DefiledSpec <defiledspec@gmail.com>
2018-08-02 19:18:47 -0500

    connected contact form to backed server, currently on local host

--- src/Controllers/ContactMsg.js
+++ src/Controllers/ContactMsg.js
@@ -2,27 +2,43 @@
 
 
 class ContactMsg {
-	constructor() {
-
+	constructor(name, email, msg) {
+		this.data = {
+			name,
+			email,
+			msg,
+		}
 	}
 	validate() {
-
+		return true
 	}
-	async add(data) {
-		const headers = new Headers()
-		headers.apppend('Content-Type', 'application/json')
-
-		const options = {
-			method: 'POST',
-			headers,
-			body: JSON.stringify(data)
-		}
-		const request = new Request('http://localhost:3001/v1/contact/add', options)
-		const res = await fetch(request)
+	async add() {
+		if (this.validate()) {
+			const headers = new Headers()
+			headers.append('Content-Type', 'application/json')
+			const options = {
+				method: 'POST',
+				headers,
+				body: JSON.stringify(this.data)
+			}
+			const request = new Request('http://localhost:3005/api/messages/add', options)
+			try {
+				const res = await fetch(request)
+				if (res.ok) {
+					// let log = await res.json()
+					console.log('Message Sent successfully', await res.json())
+					return true				
+				} else {
+					return true
+				}	
+			}catch(err) {
+				return false
+			}
+		} else {
+			return false
+		}	
 	}
-
-
 
 }
 
-export default Contact
+export default ContactMsg
